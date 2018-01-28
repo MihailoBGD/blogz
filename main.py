0ec10b6636700d4d12c7990d453856b741db24fd
MihailoBGD <mnsbgd@yahoo.com>
2018-01-28 16:21:42 -0500

    everythings working - to do user pages

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:lozinka@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:lozinka@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'secretkey'
 
 
 class Blog(db.Model):
@@ -14,13 +15,37 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     text = db.Column(db.String(600))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, text):
+    def __init__(self, title, text, owner):
         self.title = title
-        self.text = text 
+        self.text = text
+        self.owner = owner 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique = True)
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password 
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'register', 'index', 'blog']
+    if request.endpoint not in allowed_routes and 'username' not in session:
+        return redirect('/login')
+
+
+
+@app.route("/") #TODO create a page that shows all of the users
+def index():
+    return redirect("/blog")
 
 @app.route('/blog')
-def index():
+def blog():
     if request.args.get('id'):
         blog_id = request.args.get('id')
         blog = Blog.query.filter_by(id = blog_id).first()
@@ -28,12 +53,42 @@ def index():
 
 
     blogs = Blog.query.all()
-    '''
-    blogs = [blogObj1, blogObj2, blogObj3]
-    '''
+    
+    #blogs = [blogObj1, blogObj2, blogObj3]
+    
     return render_template('blog-main.html',
                             title="Build-a-blog",
                             blogs = blogs)
+                            
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            session['username'] = username
+            flash("Logged in")
+    return render_template('login.html')
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+        #todo validate
+        existing_user = User.query.filter_by(username=username).first()
+        if not existing_user:
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            return redirect('/')
+        else: 
+            return "<h1>Duplicate user</h1>"
+    return render_template('register.html')
+
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def new_blog_entry():
@@ -41,12 +96,6 @@ def new_blog_entry():
         title_error = ""
         text_error = ""
 
-        '''
-        request.form = {
-        "title": "abc",
-        "text" : "abcdef"
-        }
-        '''
         blog_title = request.form['title']
         blog_text = request.form['text']
         if not blog_title:
@@ -58,47 +107,61 @@ def new_blog_entry():
                                     title_error = title_error, 
                                     text_error = text_error)
 
-
-        blog = Blog(blog_title, blog_text)
+        user = User.query.filter_by(username=session['username']).first()
+        blog = Blog(blog_title, blog_text, user)
         db.session.add(blog)
         db.session.commit()
-        return redirect("/")
+        #return redirect(\"blog?id=\" + str(blog.id)
+        return redirect("/blog?id={id}".format(id = blog.id))
 
     return render_template('new-blog-entry.html',title="Add-a-blog")
 
-#@app.route('/single-blog', methods=['POST', 'GET'])
-#def single-blog():
-
-
-
+@app.route('/logout')
+def logout():
+    del session['username']
+    return redirect ('/')
 
 if __name__ == '__main__':
     app.run()
 
-    
+
 
 '''
-@app.route('/blog-main', methods=['POST', 'GET'])
-def blog-main():
- 
-@app.route('/delete-task', methods=['POST'])
-def delete_task():
-
-    task_id = int(request.form['task-id'])
-    task = Task.query.get(task_id)
-    task.completed = True
-    db.session.add(task)
-    db.session.commit()
-
-    return redirect('/')
 
 
-if __name__ == '__main__':
-    app.run()
 
-    if request.method == 'POST':
-        task_name = request.form['task']
-        new_task = Blog(task_name)
-        db.session.add(new_task)
-        db.session.commit()
-'''  
+
+def get_user_by_name(self, username):
+    #Get a user object from the DB, based on their username
+    user = db
+
+@app.route('/single-blog', methods=['POST', 'GET'])
+def single-blog():
+
+
+@app.route("/singleUser")
+def single_user():
+    welcome = "Not logged in"
+    if 'user' in session:
+       
+
+@app.route("/userpage")
+def userpage():
+    welcome = "Not logged in"
+    if 'user' in session:
+
+@app.route("/singleUser")
+def single_user():
+    welcome = "Not logged in"
+    if 'user' in session:
+        welcome = "Logged in as: " + session['user']
+    title = request.args.get('blog_title')
+    if title:
+        existing_blog = Blog.query.filter_by(title= title).first()
+        author = User.query.filter_by(id= existing_blog.owner_id).first()
+        return render_template("individual.html", 
+            title= existing_blog.title, body= existing_blog.body,
+            author= author.username, welcome= welcome)
+
+
+'''    
